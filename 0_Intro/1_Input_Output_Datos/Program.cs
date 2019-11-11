fb23e2d802789bd52cd0a8d77de8fd0f655b7838
Eliseo <eliseoprofe1988@gmail.com>
2019-11-11 10:10:41 +0000

    Added info about IO, Data and Arrays

---
+++ 0_Intro/1_Input_Output_Datos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace InputOutputTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                La información es almacenada en diferentes tipos de datos, esto el programa lo guarda en variables que son borradas una vez el programa se termina de ejecutar
+                Las variables pueden ser de diferentes tipos:
+                    Numéricos: int, float, double
+                                int números enteros (-5, 0, 5, ....)
+                                float números con decimales (-15.13f, 0.0f,5.0f) si decidimos ponerle valor desde el código se le añade una f al final para indicar que son números decimales.
+                                double números con decimales también.
+                    Texto: string, char
+                            string permite almacenar frases mientras que char solo un símbolo
+             */
+            int miEntero = 10;
+
+            //Las variables númericas se pueden sumar, restar, multiplicar, dividir de la siguiente manera
+            miEntero = miEntero + 5; //10 + 5 = 15
+            //En caso de querer añadir uno se puede hacer de dos formas
+            miEntero = miEntero + 1; //15+1 = 16
+            miEntero++; //16 + 1 = 17
+            //Ahora la variable miEntero tiene el valor 10+5+1+1 = 17
+
+
+            float miDecimal = 10.0f;
+
+            string miFrase = "Hola mundo";
+            //puedes añadir más texto a un string "sumando"
+            miFrase = miFrase + " esto es muy aburrido!!"; // Ahora el texto almacenado es "Hola mundo esto es muy aburrido!!"
+
+            char miChar = 'H';
+
+            //Para escribir mensajes por pantalla se usa la función WriteLine
+            Console.WriteLine("Escribe tu mensaje:");
+
+            //Para recibir mensajes del usuario se utiliza la función ReadLine
+            //es necesario almacenar el valor escrito por el usuario para trabajar
+            //con la información
+            string textoUsuario = Console.ReadLine();
+
+            //Mostramos el mensaje
+            Console.WriteLine(textoUsuario);
+        }
+    }
+}
---
+++ 0_Intro/3_Arrays/Program.cs
@@ -0,0 +1,155 @@
+﻿/*
+Autor Eliseo Martín Mora
+Este programa es un ejemplo del uso de Arrays y funciones.
+*/
+
+using System;
+
+namespace TeoriaArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			//Escribe en la terminal (pantalla) el mensaje
+            Console.WriteLine("Hello World!");
+
+			// Un array es una colección de elementos iguales, en este caso de números enteros
+			//un array se puede crear de varias formas
+			
+			//Podemos crearlo metiendo desde el código fuente los datos que queramos
+            int[] ejemplo = { 9, 15, 5, 4, 8, 10 };
+
+			//Podemos crearlo del tamaño que consideremos de la siguiente manera
+			//En una variable almacenamos el tamaño/número de elementos que tendrá el array
+            int n = 5;
+			//Nuevamente decalaramos un array de enteros, esto se hace siempre de la siguiente manera
+			// TIPO_DE_DATO[]   ----> Indica el tipo de dato que vamos a utilizar y que se trata de un array
+			// nombre de la variable
+			// se inicializa (= new) y le recordamos el tipo de dato (int[]) y dentro de los corchetes irá el tamaño/nº de elementos que tendrá nuestro array.
+            int[] myArray = new int[n];
+			// Para almacenar información en este array lo hacemos así
+			//Ponemos el nombre de la variable y los corchetes, dentro de los corchetes la posición dentro del array y finalmente le asignamos el valor considerado.
+			//Recuerda que los arrays empiezan en la posición 0.
+			myArray[3] = 5;
+
+
+			//Como tenemos varias variables que son arrays de números hemos decidido crear funciones que trabajan con arrays, de esta manera podremos mostrarlo sin necesidad de crear un código para cada array.
+            Imprimir(ejemplo);
+            Console.WriteLine("");
+			//Igualmente hemos creado funciones para obtener el Máximo, Mínimo y la Media de un ARRAY.
+            Console.WriteLine("Máximo del array: " + GetMax(ejemplo));
+            Console.WriteLine("Mínimo del array: " + GetMin(ejemplo));
+            Console.WriteLine("Media: " + GetMedia(ejemplo));
+            //Creamos array dinámico de 3 número y lo cargamos
+            int[] miArray = new int[3];
+            Cargar(miArray);
+
+            Imprimir(miArray);
+
+
+            //Matriz de muchos alumnos
+            //20 alumnos con n examenes
+			// Hasta ahora vimos que los arrays pueden ser de una dimensión, pero podemos crearlos de dos dimensiones, esto supone tener una especie de "excel".
+			// Alumno    notas               
+  			//    0     {4,5,6}
+			//    1     {0,2,1}
+			//  ...
+			//  ...
+			//    19     {7,8,9}
+            int nAlumnos = 20;
+            int nExamenes = 3;
+			//Crea un array con 20 filas, y cada fila tendría 3 notas.
+            int[,] miClase = new int[nAlumnos, nExamenes];
+            Cargar(miClase);
+        }
+
+		//Funcion para cargar un array de dos dimensiones.
+        static void Cargar(int[,] array)
+        {
+			//Recorre las 20 filas
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Notas alumno " + i);
+				//Este bucle sirve para asignar las notas de un alumno.
+                for (int j = 0; j < array.GetLength(i); j++)
+                {
+                    Console.WriteLine("Nota del examen " + i + " para el alumno " + j);
+                    array[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+		
+		//Función que carga un array de 1 dimensión
+        static void Cargar(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++) {
+                Console.WriteLine("Valor para el array en la posición " + i + " ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }  
+
+		//Función que recibe un array de números y lo muestra.
+        static void Imprimir(int[] array)
+        {
+            Console.Write("{");
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i < array.Length - 1)
+                {
+                    Console.Write(array[i] + " ,");
+                }
+                else
+                {
+                    Console.Write(array[i] + "}");
+                }
+            }
+        }
+
+		//Función que recorre un array buscando el número más grande y lo devuelve.
+        static int GetMax(int[] array)
+        {
+            int maximo = array[0];
+
+            for(int i = 0; i < array.Length;i++)
+            {
+                if (maximo < array[i])
+                {
+                    maximo = array[i];
+                }
+            }
+            return maximo;
+        }
+		
+		//Función que recorre un array buscando el número más pequeño y lo devuelve.
+
+        static int GetMin(int[] array)
+        {
+            int minimo = array[0];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (minimo > array[i])
+                {
+                    minimo = array[i];
+                }
+            }
+            return minimo;
+        }
+		//Función que calcula la media de un array de números.
+
+        static float GetMedia(int[] array)
+        {
+            float media = 0.0f;
+            for (int i = 0; i < array.Length; i++)
+            {
+                media += array[i];
+            }
+
+            media = media / array.Length;
+
+            return media;
+        }
+
+    }
+}
